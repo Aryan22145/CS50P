f94a8c6880f1983179c91d2fb58fbcae19f30a2c
Aryan Sai Boddu <aryansaiboddu4@gmail.com>
2024-04-21 11:58:37 +0530

    Add files via upload

---
+++ Meal/Meal.py
@@ -0,0 +1,20 @@
+def main():
+    time = input("What time is it? ")
+    meal = convert(time)
+
+    if 7 <= meal <= 8:
+        print("breakfast time")
+    elif 12 <= meal <= 13:
+        print("lunch time")
+    elif 18 <= meal <= 19:
+        print("dinner time")
+
+
+def convert(time):
+    hours, minutes = time.split(":")
+    hours = float(hours) + (float(minutes) / 60)
+    return hours
+
+
+if __name__ == "__main__":
+    main()
