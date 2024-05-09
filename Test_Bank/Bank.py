9dfd932a0b1bd8f1a36b2416b3548d0d665f590a
Aryan Sai Boddu <aryansaiboddu4@gmail.com>
2024-05-09 23:38:10 +0530

    Add files via upload

---
+++ Test_Bank/Bank.py
@@ -0,0 +1,23 @@
+def main():
+    greet = input("Greeting: ")
+    a = value(greet)
+    print(f"${a}")
+
+
+def value(greet):
+    x = greet.strip().lower()
+    if x[0:5] == "hello":
+        y = 0
+        return y
+    elif x[0] == "h":
+        y = 20
+        return y
+    else:
+        x = 100
+        return x
+
+
+
+
+if __name__ == "__main__":
+    main()
